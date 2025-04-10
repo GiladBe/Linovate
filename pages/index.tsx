6c24eb07c084bb5a59e3b74ebe1bbea57c4dd001
DESKTOP-04D9UG6\Gilad <giladbeer90@gmail.com>
2025-04-10 12:33:22 +0300

    added spinnerto main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { GetServerSideProps, NextPage } from 'next';
 import axios from 'axios';
 import ProductCard from '../components/ProductCard';
 import { ProductInterface, ReviewInterface } from '../globals/types'
+import {Spinner} from '../globals/components'; 
+import {useState,useEffect} from 'react'
 
 
 
@@ -10,16 +12,22 @@ interface HomePageProps {
 }
 
 const HomePage: NextPage<HomePageProps> = ({ products }) => {
-  
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    if (products.length > 0) {
+      setLoading(false);
+    }
+  }, [products]);
  
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-3xl font-bold mb-6">Product Catalog</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {products.map((product) => (
+         {!loading? products.map((product) => (
           <ProductCard key={product.id} product={product} />
-        ))}
+        )):  <Spinner /> }
+        
       </div>
     </div>
   );
